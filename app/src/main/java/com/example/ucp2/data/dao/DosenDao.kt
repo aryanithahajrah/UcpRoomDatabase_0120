f1711a735fdbb87da31e7b86a74e49e024e33dd9
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-16 17:31:16 +0700

    fungsi untuk menambah dan membaca data dosen dari tabel dosen

---
+++ app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DosenDao {
+    @Query("select * from dosen ORDER BY nama ASC")
+    fun getAllDosen() : Flow<List<Dosen>>
+
+    @Query("select * from dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String) : Flow<Dosen>
+
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
+}
