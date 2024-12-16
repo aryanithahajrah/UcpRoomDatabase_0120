9eb2e71b84b9da04a16614f86c1c2993cdff2d1d
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-16 17:33:08 +0700

    database yang menyediakan akses ke tabel Dosen dan Matakuliah melalui DosenDao dan MatakuliahDao

---
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MatakuliahDao
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.Matakuliah
+
+@Database(entities = [Dosen::class, Matakuliah::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase() {
+    // Mendefinisikan fungsi untuk mengakses data Dosen
+    abstract fun DosenDao(): DosenDao
+
+    // Mendefinisikan fungsi untuk mengakses data Matakuliah
+    abstract fun MatakuliahDao(): MatakuliahDao
+
+    companion object {
+        @Volatile // Memastikan bahwa nilai variabel Instance selalu sama di semua thread
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java, // Class database
+                    "KrsDatabase" // Nama database
+                )
+                    .build().also { Instance = it }
+            }
+        }
+    }
+
+}
+
