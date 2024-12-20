e8770dcba93d3fe1f95e867c4ae50222f4d290c8
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-20 19:05:27 +0700

    class yang menyediakan akses ke data dosen dan matakuliah dengan menghubungkan repository ke database lokal KrsDatabase

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,1 +1,23 @@
 package com.example.ucp2.dependenciesinjection
+
+import android.content.Context
+import com.example.ucp2.data.database.KrsDatabase
+import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMatakuliah
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMatakuliah
+
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+    val repositoryMatakuliah: RepositoryMatakuliah
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).DosenDao())
+    }
+
+    override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
+        LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).MatakuliahDao())
+    }
+}
